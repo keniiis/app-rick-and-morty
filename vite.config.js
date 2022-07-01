dc93742a614b9c5392390682bd5462ca586a6e56
keniiis <daniel.pena@codingbox.clgit config --global user.name keniiisgit config --global user.email daniel.pena@codingbox.clgit config --global user.email daniel.pena@codingbox.cl>
2022-07-01 19:57:08 -0400

    arreglos deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/app-rick-and-morty',
+  base: 'https://keniiis.github.io/app-rick-and-morty',
   plugins: [react()]
 })
